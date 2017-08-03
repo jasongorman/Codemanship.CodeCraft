219b2494375c51c63c33ecd689221ff8d54da8fd
Jason Gorman <jason.gorman@codemanship.com>
2017-08-03 18:24:03 +0100

    Tweaked - investigating Refused Bequest with ICodeObject implementations

---
+++ Codemanship.CodeCraft.Tests/Integration/AssemblyLoaderTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Codemanship.CodeCraft.CecilWrappers;
+using NUnit.Framework;
+
+namespace Codemanship.CodeCraft.Tests.Integration
+{
+    [TestFixture]
+    public class AssemblyLoaderTests
+    {
+        [Test]
+        public void LoadsTestAssembly()
+        {
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\SampleCode.dll";
+            IAssembly assembly = new AssemblyLoader().Load(path);
+            Assert.That(assembly.Name.Contains("SampleCode"));
+        }
+    }
+}
